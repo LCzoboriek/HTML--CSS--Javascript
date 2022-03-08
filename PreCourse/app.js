2408467be6953fe803ef9bf94da21c8ac3a6589e
Luke Czoboriek <LC169782@protonmail.com>
2022-03-08 22:25:55 +0000

    added loops

--- PreCourse/app.js
+++ PreCourse/app.js
@@ -21,3 +21,31 @@ console.log(array.length);
 array = [true, "Yes", 1]
 
 
+let count = 0;
+let max = 10;
+
+// while(count < max) {
+//     console.log(count);
+//     count = count + 1;
+// }
+
+for (let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+let numbers = [true, 200, "Hello"];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+// How to iterate through an array and clg all the values in the array until you reach the length
+// of the numbers
+
+// For each iterates over an array one by one
+// this is used instead of doing a for loop
+let days = ["monday", "tuesday", "wednesday"];
+
+days.forEach(function(day, index){
+    console.log(`day: ${day}, index: ${index}`);
+})
